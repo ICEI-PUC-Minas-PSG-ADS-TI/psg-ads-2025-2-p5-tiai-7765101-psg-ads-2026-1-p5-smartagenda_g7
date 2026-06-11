097a2bc5d4c2828dc61eb94425334b33495735db
Maie-r <nitainandi1@gmail.com>
2026-06-11 15:44:22 -0300

    Alterado nome do projeto para SmartAgenda

--- src/app/ios/SmartAgenda/AppDelegate.swift
+++ src/app/ios/SmartAgenda/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "LocalAIProof",
+      withModuleName: "SmartAgenda",
       in: window,
       launchOptions: launchOptions
     )
